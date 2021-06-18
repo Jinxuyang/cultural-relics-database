9ac777a8ec797f62a6934f63d223683a3102e2a3
Verge <981340404@qq.com>
2021-06-18 19:19:15 +0800

    chore(global): init repo

---
+++ src/main/java/com/fehead/culturalrelicsdatabase/CulturalRelicsDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.fehead.culturalrelicsdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalRelicsDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulturalRelicsDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fehead/culturalrelicsdatabase/CulturalRelicsDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fehead.culturalrelicsdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalRelicsDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
