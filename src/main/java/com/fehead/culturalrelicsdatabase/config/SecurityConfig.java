5e5107fbf27efc47cc191d6647445f2b06aa1eb8
zero <1444171773@qq.com>
2021-06-19 15:04:06 +0800

    feat(global): modify login

--- src/main/java/com/fehead/culturalrelicsdatabase/config/SecurityConfig.java
+++ src/main/java/com/fehead/culturalrelicsdatabase/config/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.annotation.Resource;
-import javax.sql.DataSource;
+
 
 
 /**
